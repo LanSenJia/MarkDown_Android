4434600321b8c4f0761176c8480ce2795f20dc8f
ddsjls@126.com <5201314hyf>
2018-12-18 20:40:16 +0800

    1.删删减减了一些东西

--- app/src/main/java/com/lansenboy/markdown_android/MainActivity.java
+++ app/src/main/java/com/lansenboy/markdown_android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lansenboy.markdown_android;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        toolbar.setTitle("Markdown_Android");
+        toolbar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+
     }
 }
